bd76e3599b4c5cb815bc6c860ace04384886acc9
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2023-04-15 15:27:29 +1000

    inject part 2

--- Chrome/src/background.js
+++ Chrome/src/background.js
@@ -1,3 +1,14 @@
 chrome.webRequest.onCompleted.addListener((rqst) => {
-	chrome.tabs.query({active: true, lastFocusedWindow: true }).then(tablist => {if(tablist.length == 1){console.log(tablist[0].id)}});
+	chrome.tabs.query({active: true, lastFocusedWindow: true }).then(
+		tablist => {
+			if(tablist.length == 1){
+				chrome.scripting.executeScript(
+					{
+						target: {tabId: tablist[0].id},
+						files: ["src/webp2png.js"]
+					}
+				)
+			}
+		}
+	);
 }, {urls: ["https://*/*.webp"]});
