f4e7acb93e57bdb41b82e6e25800e219fe9af712
Sid Shardanand <51441497+s1ddly@users.noreply.github.com>
2023-04-15 15:57:35 +1000

    starting on the request logic

--- Chrome/src/background.js
+++ Chrome/src/background.js
@@ -2,13 +2,16 @@ chrome.webRequest.onCompleted.addListener((rqst) => {
 	chrome.tabs.query({active: true, lastFocusedWindow: true }).then(
 		tablist => {
 			if(tablist.length == 1){
+				//DEBUG
+				console.log(rqst.url);
+				//END OF DEBUG
 				chrome.scripting.executeScript(
 					{
-						target: {tabId: tablist[0].id},
+						target: {tabId: tablist[0].id, allFrames : true},
 						files: ["src/webp2png.js"]
 					}
 				)
 			}
 		}
 	);
-}, {urls: ["https://*/*.webp"]});
+}, {urls: ["https://*/*.webp", "http://*/*.webp"]});
--- Chrome/src/webp2png.js
+++ Chrome/src/webp2png.js
@@ -1,1 +1,8 @@
-console.log("triggered");
+//console.log("triggered");
+imglist = document.getElementsByTagName("img");
+for (var i = 0; i < imglist.length; i ++){
+	//console.log(imglist[i].src.substr(imglist[i].src.length - 5).toLowerCase());
+	if(imglist[i].src.substr(imglist[i].src.length - 5).toLowerCase() == ".webp"){
+		console.log(imglist[i].src);
+	}
+}
